5fc0214e8fd57d4b7699ef35d2201e82e2781236
Mehdi Salimi <Salimi.Devop@gmail.com>
2022-03-03 00:20:23 +0330

    Added error messages to show in HTML and UI

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,61 +3,103 @@ import styles from './SingUp.module.css';
 import { validate } from './validate';
 
 const SignUp = () => {
-     const [inputData, setInputData] = useState({
-          name: '',
-          email: '',
-          password: '',
-          confirmPassword: '',
-          isAccepted: false,
-     });
-     const [errors, setErrors] = useState({});
-     useEffect(() => {
-          setErrors(validate(inputData));
-     }, [inputData]);
-     // Destructure object
-     const { name, email, password, confirmPassword, isAccepted } = inputData;
+      //States
+      const [inputData, setInputData] = useState({
+            name: '',
+            email: '',
+            password: '',
+            confirmPassword: '',
+            isAccepted: false,
+      });
+      const [errors, setErrors] = useState({});
+      const [touched, setTouched] = useState({});
+      //Get inputs errors from validate component
+      useEffect(() => {
+            setErrors(validate(inputData));
+      }, [inputData, touched]);
+      const setFieldTouched = (e) => {
+            setTouched({ ...touched, [e.target.name]: true });
+      };
+      // Destructure object
+      const { name, email, password, confirmPassword, isAccepted } = inputData;
+      //Handle inputs updating
+      const inputOnChange = (e) => {
+            if (e.target.name === 'isAccepted') {
+                  setInputData({ ...inputData, [e.target.name]: e.target.checked });
+            } else {
+                  setInputData({ ...inputData, [e.target.name]: e.target.value });
+            }
+      };
+      const formOnSubmit = (e) => {
+            e.preventDefault();
+            if (Object.keys(errors).length) {
+                  setTouched({
+                        name: true,
+                        email: true,
+                        password: true,
+                        confirmPassword: true,
+                        isAccepted: true,
+                  });
+            }
+      };
 
-     const inputOnChange = (e) => {
-          if (e.target.name === 'isAccepted') {
-               setInputData({ ...inputData, [e.target.name]: e.target.checked });
-          } else {
-               setInputData({ ...inputData, [e.target.name]: e.target.value });
-          }
-     };
-     return (
-          <div>
-               <div className={styles.container}>
-                    <form className={styles.formContainer}>
-                         <h2 className={styles.header}>SignUp</h2>
-                         <div className={styles.formField}>
-                              <label>Name</label>
-                              <input type='text' name='name' value={name} onChange={inputOnChange} />
-                         </div>
-                         <div className={styles.formField}>
-                              <label>Email</label>
-                              <input type='email' name='email' value={email} onChange={inputOnChange} />
-                         </div>
-                         <div className={styles.formField}>
-                              <label>Password</label>
-                              <input type='password' name='password' value={password} onChange={inputOnChange} />
-                         </div>
-                         <div className={styles.formField}>
-                              <label>Confirm Password</label>
-                              <input type='password' name='confirmPassword' value={confirmPassword} onChange={inputOnChange} />
-                         </div>
-                         <div className={styles.formField}>
-                              <div className={styles.checkboxContainer}>
-                                   <label>I accept terms of privacy policy</label>
-                                   <input type='checkbox' name='isAccepted' value={isAccepted} onChange={inputOnChange} />
+      return (
+            <div>
+                  <div className={styles.container}>
+                        <form className={styles.formContainer} onSubmit={formOnSubmit}>
+                              <h2 className={styles.header}>SignUp</h2>
+                              <div className={styles.formField}>
+                                    <label>Name</label>
+                                    <input type='text' name='name' value={name} onChange={inputOnChange} onFocus={setFieldTouched} />
+                                    {errors.name && touched.name && <span>{errors.name}</span>}
                               </div>
-                         </div>
-                         <div className={styles.buttonsContainer}>
-                              <button type='submit'>Sign Up</button>
-                         </div>
-                    </form>
-               </div>
-          </div>
-     );
+                              <div className={styles.formField}>
+                                    <label>Email</label>
+                                    <input type='email' name='email' value={email} onChange={inputOnChange} onFocus={setFieldTouched} />
+                                    {errors.email && touched.email && <span>{errors.email}</span>}
+                              </div>
+                              <div className={styles.formField}>
+                                    <label>Password</label>
+                                    <input
+                                          type='password'
+                                          name='password'
+                                          value={password}
+                                          onChange={inputOnChange}
+                                          onFocus={setFieldTouched}
+                                    />
+                                    {errors.password && touched.password && <span>{errors.password}</span>}
+                              </div>
+                              <div className={styles.formField}>
+                                    <label>Confirm Password</label>
+                                    <input
+                                          type='password'
+                                          name='confirmPassword'
+                                          value={confirmPassword}
+                                          onChange={inputOnChange}
+                                          onFocus={setFieldTouched}
+                                    />
+                                    {errors.confirmPassword && touched.confirmPassword && <span>{errors.confirmPassword}</span>}
+                              </div>
+                              <div className={styles.formField}>
+                                    <div className={styles.checkboxContainer}>
+                                          <label>I accept terms of privacy policy</label>
+                                          <input
+                                                type='checkbox'
+                                                name='isAccepted'
+                                                value={isAccepted}
+                                                onChange={inputOnChange}
+                                                onFocus={setFieldTouched}
+                                          />
+                                          {errors.isAccepted && touched.isAccepted && <span>{errors.isAccepted}</span>}
+                                    </div>
+                              </div>
+                              <div className={styles.buttonsContainer}>
+                                    <button type='submit'>Sign Up</button>
+                              </div>
+                        </form>
+                  </div>
+            </div>
+      );
 };
 
 export default SignUp;
