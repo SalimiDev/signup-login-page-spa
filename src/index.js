3a592866538762e4f1f64be4389968f5463814d2
Mehdi Salimi <Salimi.devop@gmail.com>
2023-04-19 06:34:41 +0330

    update index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 ReactDOM.render(
-      <BrowserRouter>
+      <BrowserRouter basename='/signup-login-page-spa'>
             <App />
       </BrowserRouter>,
       document.getElementById('root'),
